4d6b151f88d4a5b05e373ca0fba84f6963b1d0e5
Abdulazizkarma1 <alhassanabdulaziz96@gmail.com>
2025-06-30 23:03:42 +0000

    Refactor MongoDB connection: simplify connection code by removing deprecated options for cleaner implementation

--- server/index.js
+++ server/index.js
@@ -32,10 +32,8 @@ app.use(express.json());
 
 app.use('/api', notificationRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
